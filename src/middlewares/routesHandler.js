ee8114b249d3cc5415af39b26d3f6c73f07a8d49
Fern <fmenezes098@gmail.com>
2026-01-14 11:57:21 -0300

    :construction: chore: Middleware de rotas

---
+++ src/middlewares/routesHandler.js
@@ -0,0 +1,12 @@
+import { routes } from "../routes/index.js";
+
+export async function routeHandler(request, response){
+    const route = routes.find((route)=>{
+        return route.method === request.method && route.path === request.url
+    })
+
+    if(route){
+        return route.controller(request, response)
+    } 
+    return response.writeHeader(404).end("Not Found")
+}
