05fd8d2c2b22e7d9a01e816904a9b708dbea5408
Fern <fmenezes098@gmail.com>
2026-01-19 11:42:03 -0300

    :mag: filter: Filtrando pelo status

--- src/database/database.js
+++ src/database/database.js
@@ -23,8 +23,19 @@ export class Database {
         return data
     }
 
-    select(table) {
+    select(table, filters) {
         let data = this.#database[table] ?? []
+
+        if(filters){
+            data = data.filter((row) => {
+               return Object.entries(filters).some(([key, value]) => {
+                    return row[key].toLowerCase().includes(value.toLowerCase())
+                })
+                //console.log(test)
+            })
+
+            
+        }
         return data
     }
 }
